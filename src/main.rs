41e6289138e93b7cba85434d0e5cd36edbaf9be4
Joonas Sarajärvi <muep@iki.fi>
2022-01-24 21:55:56 +0200

    Add command line parser and some stub code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::{App, AppSettings};
+
 fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Produce issue lists from git logs")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(App::new("check-jira").about("Check connectivity with Jira"))
+        .get_matches();
     println!("Hello, world!");
 }
