347be3598fbd798a419b20d4280e646d9bf8b0fa
Joonas Sarajärvi <muep@iki.fi>
2022-02-01 22:32:35 +0200

    Working fetch of Jira issue summary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use clap::{arg, App, AppSettings};
+use serde::Deserialize;
 use std::fs::read_to_string;
 
-use clap::{arg, App, AppSettings};
+#[derive(Debug, Deserialize)]
+struct Fields {
+    summary: String,
+}
 
+#[derive(Debug, Deserialize)]
+struct Issue {
+    fields: Fields,
+}
+
+const SC_ISSUE_SUMMARY: &str = "jira-issue-summary";
 const SC_CHECK_JIRA: &str = "check-jira";
 
 async fn check_jira(url: &str, user: &str, token: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -20,6 +31,25 @@ async fn check_jira(url: &str, user: &str, token: &str) -> Result<(), Box<dyn st
     Ok(())
 }
 
+async fn issue_summary(
+    url: &str,
+    user: &str,
+    token: &str,
+    issue: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let body = client
+        .get(format!("{}/rest/api/3/issue/{}", url, issue))
+        .basic_auth(user, Some(token))
+        .send()
+        .await?
+        .json::<Issue>()
+        .await?;
+
+    Ok(body.fields.summary)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -36,6 +66,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .arg(arg!(<URL> "Jira URL"))
                 .arg(arg!(<USER> "User account (email address) to Jira")),
         )
+        .subcommand(
+            App::new(SC_ISSUE_SUMMARY)
+                .about("Get an issue summary from Jira")
+                .arg(
+                    arg!(-t --token <TOKEN> "Path to file with Personal access token")
+                        .required(true),
+                )
+                .arg(arg!(<URL> "Jira URL"))
+                .arg(arg!(<USER> "User account (email address) to Jira"))
+                .arg(arg!(<ISSUE> "Jira issue")),
+        )
         .get_matches();
 
     match matches.subcommand() {
@@ -47,6 +88,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 token.trim(),
             )
             .await
+        }
+        Some((SC_ISSUE_SUMMARY, sub_matches)) => {
+            let token = read_to_string(sub_matches.value_of("token").unwrap()).unwrap();
+            let issue = sub_matches.value_of("ISSUE").unwrap();
+            let summary = issue_summary(
+                sub_matches.value_of("URL").unwrap(),
+                sub_matches.value_of("USER").unwrap(),
+                token.trim(),
+                issue,
+            )
+            .await?;
+
+            println!("{} {}", issue, summary);
+
+            Ok(())
         }
         _ => Ok(()),
     }
